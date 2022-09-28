644d4b5d830f89cdf75f642cab531136e352a1f9
Alok Pandey <palok627@gmail.com>
2022-09-28 17:45:58 +0530

    Electricity bill

---
+++ electricitybill.c
@@ -0,0 +1,31 @@
+/*input electricity unit charges and calculate total electricity bill according to the given condition:
+For first 50 units Rs. 0.50/unit
+For next 100 units Rs. 0.75/unit
+For next 100 units Rs. 1.20/unit
+For unit above 250 Rs. 1.50/unit
+An additional surcharge of 20% is added to the bill*/
+
+ #include<stdio.h>
+ int main()
+ {
+    float unit;
+    printf("\nEnter the electricity unit charges ");
+    scanf("%f",&unit);
+    if(unit >=1 && unit <= 50)
+    {
+        printf("The total electricity bill is %f ",unit*0.50);
+    }
+    else if(unit>50 && unit<=150 ){
+        printf("The total electricity bill is %f",unit*0.75);
+    }
+    else if(unit>150 && unit <=250)
+    {
+        printf("The total electricity bill is %f", unit*1.20);
+    }
+    else
+    {
+        printf("The total electricity bill is %f", unit*1.50 +(unit*1.50)*0.20);
+    }
+
+    return 0;
+ }
